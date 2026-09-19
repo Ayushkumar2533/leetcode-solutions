f1a94ec0ee9efd0a42304a69b069d293fc86b68e
Ayush kumar <ayushkumar2533@gmail.com>
2026-09-19 23:36:26 +0530

    Time: 0 ms (100%), Space: 64.6 MB (70.14%) - LeetHub

---
+++ C++/Easy/2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxCount = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < mat[i].size(); j++) {
+                if (mat[i][j] == 1) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxIndex = i;
+            }
+        }
+        return {maxIndex, maxCount};
+    }
+};
