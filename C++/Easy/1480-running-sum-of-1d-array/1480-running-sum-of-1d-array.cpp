d87595931b1b43319dff34f8bf331c9028457665
Ayush kumar <ayushkumar2533@gmail.com>
2026-09-20 22:35:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (81.28%) - LeetHub

--- C++/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ C++/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         int n = nums.size();
-        vector<int>Sum(n);
-        partial_sum(nums.begin(),nums.end(),Sum.begin());
-       return Sum;
+        vector<int> Sum(n);
+        partial_sum(nums.begin(), nums.end(), Sum.begin());
+        return Sum;
     }
 };
